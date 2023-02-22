04c73be13e8568be9610752a1ae3b47a820e85f3
yunsuklee <sergioleesp@gmail.com>
2023-02-22 12:06:25 +0100

    bloglist 5.10

--- bloglist-frontend/src/components/BlogList.js
+++ bloglist-frontend/src/components/BlogList.js
@@ -85,14 +85,16 @@ const BlogList = (props) => {
           addBlog={addBlog}
         />
       </Togglable>
-      {blogs.map(blog =>
-        <Blog
-          key={blog.id}
-          blog={blog}
-          setMessage={props.setMessage}
-          setMessageType={props.setMessageType}
-        />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog
+            key={blog.id}
+            blog={blog}
+            setMessage={props.setMessage}
+            setMessageType={props.setMessageType}
+          />
+        )}
     </div>
   )
 }
